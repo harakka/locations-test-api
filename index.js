50ec18f94497d284b12f7f30022d1afb0601a45e
harakka <harakka@myrskynkantaja.org>
2021-03-03 10:56:35 +0200

    location api v1

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 },
+];
+
+app.get("/api/locations", (req, res) => {
+  res.send(database);
+});
